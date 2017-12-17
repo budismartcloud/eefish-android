461f50f38031dfd91cb2e4d17706d19d25355f20
nadia ningtias <nadia.ningtias20@gmail.com>
2017-12-17 12:50:55 +0700

    commit lanjutan adapter history

--- app/src/main/java/com/example/user/eefish/Controller/AppConfig.java
+++ app/src/main/java/com/example/user/eefish/Controller/AppConfig.java
@@ -8,4 +8,6 @@ public class AppConfig {
     public static final String URL_FISH_BY_ID = ROOT_URL + "api/fish/filter?fish=";
     public static final String ADD_TO_CART= ROOT_URL + "api/cart/add-to-cart";
     public static final String URL_LIST_CART = ROOT_URL + "api/cart/";
+
+    public static final String URL_ORDER_ON_GOING = ROOT_URL + "api/cart/";
 }
--- app/src/main/java/com/example/user/eefish/Fragment/HistoryOnProgressFragment.java
+++ app/src/main/java/com/example/user/eefish/Fragment/HistoryOnProgressFragment.java
@@ -9,9 +9,21 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.example.user.eefish.Controller.AppConfig;
+import com.example.user.eefish.Controller.AppController;
 import com.example.user.eefish.Controller.SessionManager;
+import com.example.user.eefish.Model.History;
 import com.example.user.eefish.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -23,6 +35,7 @@ import java.util.HashMap;
 
 public class HistoryOnProgressFragment extends Fragment {
 
+    static String URL_ORDER_HISTORY_ON_PROGRESS ;
     RecyclerView rvHistory;
     SessionManager session;
     private ArrayList<HashMap<String, String>> listHistoryOnProgress;
@@ -43,8 +56,70 @@ public class HistoryOnProgressFragment extends Fragment {
         getHistoryOrderOnProgress(session.getIdUser());
         return rootView;
     }
+//    public void getHistoryOrderOnProgress(int idUser) {
+//        URL_ORDER_HISTORY_ON_PROGRESS = ""+ AppConfig.URL_ORDER_ON_GOING + String.valueOf(idUser).toString();
+//
+//        final StringRequest stringRequest = new StringRequest(
+//                Request.Method.GET,
+//                URL_ORDER_HISTORY_ON_PROGRESS,
+//                new Response.Listener<String>(){
+//                    @Override
+//                    public void onResponse(String response) {
+//                        try{
+//                            //convert response to json object
+//                            JSONObject obj = new JSONObject(response);
+//
+//                            //302 = no error on response data
+//                            if (obj.getInt("code") == 302) {
+////                                Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+//                                //getting the fish from the response
+//                                JSONArray jsonArrayFish = obj.getJSONArray("data");
+//                                for (int a = 0; a < jsonArrayFish.length();a++){
+//                                    JSONObject objFish = jsonArrayFish.getJSONObject(a);
 
-    private void getHistoryOrderOnProgress(int idUser) {
-
-    }
-}
+//                                    History fish = new Fish(
+//                                            objFish.getInt("fish_id"),
+//                                            objFish.getString("weight"),
+//                                            objFish.getInt("variety_id"),
+//                                            objFish.getString("quantity"),
+//                                            objFish.getString("category_id"),
+//                                            objFish.getString("fish_name"),
+//                                            objFish.getString("variety_name"),
+//                                            objFish.getString("singular_price"),
+//                                            objFish.getString("collective_price"),
+//                                            objFish.getString("category_name"),
+//                                            objFish.getString("picture")
+//                                    );
+//                                    HashMap<String,String> mapIkan = new HashMap<String,String>();
+//                                    mapIkan.put("id",String.valueOf(fish.getFish_id()));
+//                                    mapIkan.put("fish_name",fish.getFish_name());
+//                                    mapIkan.put("colective_price",fish.getColective_price());
+//                                    mapIkan.put("singular_price",fish.getSingular_price());
+//                                    mapIkan.put("picture",fish.getPicture_fish());
+////
+//                                    listDataIkanFiltered.add(mapIkan);
+//                                    // ADAPTERVIEW KE LISTVIEW HINGGA SET ADAPTER
+//                                    ListAdapterIkan listAdapterIkanIkan = new ListAdapterIkan(KategoriActivity.this,listDataIkanFiltered );
+//                                    lvIkan.setAdapter(listAdapterIkanIkan);
+//                                }
+//                            }
+//                            else {
+//                                Toast.makeText(KategoriActivity.this, "message", Toast.LENGTH_SHORT).show();
+//                            }
+//
+//                        }catch (JSONException ex){
+//                            ex.printStackTrace();
+//                        }
+//
+//                    }
+//                },new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+//            }
+//        }){
+//            //TODO NAMPILKAN HASMAP DARI VARIABBLE NULL DARI GET DATA WEB
+//        };
+//        AppController.getInstance(this).addToRequestQueue(stringRequest);
+//    }
+//}
