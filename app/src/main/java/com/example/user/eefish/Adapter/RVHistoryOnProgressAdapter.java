e0a16055376a19ad3458f9cdb09d520947ff6bef
nadia ningtias <nadia.ningtias20@gmail.com>
2017-12-17 12:39:26 +0700

    commit adapter history

--- app/src/main/java/com/example/user/eefish/Adapter/RVHistoryOnProgressAdapter.java
+++ app/src/main/java/com/example/user/eefish/Adapter/RVHistoryOnProgressAdapter.java
@@ -1,11 +1,17 @@
 package com.example.user.eefish.Adapter;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.user.eefish.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Nadian on 12/16/2017.
@@ -13,27 +19,41 @@ import com.example.user.eefish.R;
 
 public class RVHistoryOnProgressAdapter extends RecyclerView.Adapter<RVHistoryOnProgressAdapter.ViewHolder> {
 
+    private Context mContext;
+    private ArrayList<HashMap<String,String >> mListDataHistoryOnprogress;
+    private Bundle mBundle;
+
+    public RVHistoryOnProgressAdapter(Context mContext, ArrayList<HashMap<String, String>> mListDataHistoryOnprogress) {
+        this.mContext = mContext;
+        this.mListDataHistoryOnprogress = mListDataHistoryOnprogress;
+    }
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_in_history,null);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        holder.waktuOrder.setText(mListDataHistoryOnprogress.get(position).get("time"));
+        holder.tanggalOrder.setText(mListDataHistoryOnprogress.get(position).get("date"));
+        holder.alamatOrder.setText(mListDataHistoryOnprogress.get(position).get("address"));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mListDataHistoryOnprogress.size();
     }
 
     public class ViewHolder extends  RecyclerView.ViewHolder
     {
         TextView alamatOrder, tanggalOrder, waktuOrder;
         public ViewHolder(View itemView) {
-            alamatOrder = itemView.findViewById(R.id.)
             super(itemView);
+            alamatOrder = itemView.findViewById(R.id.tv_history_onprogress_address);
+            tanggalOrder= itemView.findViewById(R.id.tv_history_onprogress_date);
+            waktuOrder= itemView.findViewById(R.id.tv_history_onprogress_date);
         }
     }
 
--- app/src/main/java/com/example/user/eefish/Fragment/HistoryOnProgressFragment.java
+++ app/src/main/java/com/example/user/eefish/Fragment/HistoryOnProgressFragment.java
@@ -4,6 +4,7 @@ package com.example.user.eefish.Fragment;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +13,9 @@ import android.view.ViewGroup;
 import com.example.user.eefish.Controller.SessionManager;
 import com.example.user.eefish.R;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 
 /**
  * Created by Nadian on 12/16/2017.
@@ -21,7 +24,8 @@ import java.util.Calendar;
 public class HistoryOnProgressFragment extends Fragment {
 
     RecyclerView rvHistory;
-    SessionManager sessionManager;
+    SessionManager session;
+    private ArrayList<HashMap<String, String>> listHistoryOnProgress;
 
 
     @Override
@@ -29,10 +33,18 @@ public class HistoryOnProgressFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.tab_history_fragment,container,false);
         final Calendar calendar = Calendar.getInstance();
-        sessionManager = new SessionManager(getActivity());
+        session = new SessionManager(getActivity());
+        listHistoryOnProgress = new ArrayList<HashMap<String,String>>();
 
         rvHistory = rootView.findViewById(R.id.rv_tab_history_fragment);
+        LinearLayoutManager llmHistoryOnProgress = new LinearLayoutManager(rootView.getContext());
 
+
+        getHistoryOrderOnProgress(session.getIdUser());
         return rootView;
+    }
+
+    private void getHistoryOrderOnProgress(int idUser) {
+
     }
 }
